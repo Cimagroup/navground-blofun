d1128c7f942391fc348ea35f336c0b4caf0a6e86
Alvaro Torras Casas <atorras1618@gmail.com>
2024-08-21 18:11:13 +0200

    consecutive timestep distance for torus_2D

--- perdiver/distances.py
+++ perdiver/distances.py
@@ -1,11 +1,28 @@
 import scipy.spatial.distance as dist
 import numpy as np
 
+def check_points_velocities(points, velocities):
+    if (not np.all(points.shape==velocities.shape)) or (points.shape[1]!=2):
+        raise(ValueError)
+        
 def distances_weighted_velocities(points, velocities, weight):
+    """ This function takes positions and velocities of a moving point cloud at a given time and returns 
+    the distance matrix where velocities are weighted by the parameter `weight`. The metric space 
+    corresponds to points given as four coordinates (x,y,wv_x, wv_y) where `w`is the weight, `x`, `y` are the coordinates
+    and `v_x`, `v_y` are the vector components.
+    """
+    check_points_velocities(points, velocities)
     points_velocities = np.hstack((points, velocities*weight))
     return dist.squareform(dist.pdist(points_velocities, "minkowski", p=2))
 
 def distances_corridor_weighted_velocities(points, velocities, weight, length):
+    """ Returns the distance matrix of points in a corridor. Velocities are weighted by the parameter `weight`.
+    The metric space corresponds to points given as four coordinates (x, y, wv_x, wv_y) where `w`is the weight, 
+    `x`, `y` are the coordinates and `v_x`, `v_y` are the vector components.
+    This requieres a specific function because of the periodic boundary conditions.
+    `length` is the corridor length.
+    """
+    check_points_velocities(points, velocities)
     points_velocities = np.hstack((points, velocities*weight))
     dist_0 = dist.squareform(dist.pdist(points_velocities, "minkowski", p=2))
     shift_points_vels = np.array(points_velocities) # make a copy
@@ -13,6 +30,58 @@ def distances_corridor_weighted_velocities(points, velocities, weight, length):
     shift_points_vels[left_pts_idx] += [length,0,0,0]
     dist_1 = dist.squareform(dist.pdist(shift_points_vels, "minkowski", p=2))
     return np.minimum(dist_0, dist_1)
+
+def distances_2Dtorus_weighted_velocities(points, velocities, weight, side):
+    """ Returns the distance matrix of points in a square with side `side` and with periodic
+    boundary conditions, so that it is a torus. Velocities are weighted by the parameter `weight`.
+    The metric space corresponds to points given as four coordinates (x, y, wv_x, wv_y) where `w`is the weight, 
+    `x`, `y` are the coordinates and `v_x`, `v_y` are the vector components.
+    This requieres a specific function because of the periodic boundary conditions.
+    """
+    check_points_velocities(points, velocities)
+    ####
+    positions_velocities_list = []
+    # Horizontal shift [side, 0] 
+    points_shift = np.array(points)
+    points_shift[points_shift[:,0] < (side/2)] += [side, 0]
+    positions_velocities_list.append(np.hstack((points_shift, velocities*weight)))
+    # Vertical shift [0, side]
+    points_shift = np.array(points)
+    points_shift[points_shift[:,1] < (side/2)] += [0, side]
+    positions_velocities_list.append(np.hstack((points_shift, velocities*weight)))
+    # Diagonal shift [side, side]
+    points_shift = np.array(points)
+    # Take into account torus periodicity
+    points_shift[np.logical_and(points_shift[:,0] <  (side/2), points_shift[:,1] < (side/2))]  += [side, side]
+    points_shift[np.logical_and(points_shift[:,0] <  (side/2), points_shift[:,1] >= (side/2))] += [side, 0]
+    points_shift[np.logical_and(points_shift[:,0] >= (side/2), points_shift[:,1] < (side/2))]  += [0, side]
+    positions_velocities_list.append(np.hstack((points_shift, velocities*weight)))
+    ### Now, compute the distances for each instance of points with applied periodic conditions
+    distances_list = []
+    for idx, points_vel in enumerate(positions_velocities_list):
+        # Compare trajectories at same time
+        points_vel_compare = positions_velocities_list[idx]
+        distances_list.append(dist.cdist(points_vel, points_vel_compare, "minkowski", p=2))
+    # end for
+    #### Take minimum distances and return
+    distances_arr = np.array(distances_list)
+    return np.min(distances_arr, axis=0)
+
+### Functions that return distance matrices corresponding to a pair of timesteps
+
+def compute_distance_matrices_timesteps_corridor(X, Y, vel_X, vel_Y, weight, length):
+    Dist_X = distances_corridor_weighted_velocities(X, vel_X, weight, length)
+    Dist_Y = distances_corridor_weighted_velocities(Y, vel_Y, weight, length)
+    Dist_Z = np.minimum(Dist_X, Dist_Y)
+    return Dist_X, Dist_Y, Dist_Z
+
+def compute_distance_matrices_timesteps_2Dtorus(X, Y, vel_X, vel_Y, weight, side):
+    Dist_X = distances_2Dtorus_weighted_velocities(X, vel_X, weight, side)
+    Dist_Y = distances_2Dtorus_weighted_velocities(Y, vel_Y, weight, side)
+    Dist_Z = np.minimum(Dist_X, Dist_Y)
+    return Dist_X, Dist_Y, Dist_Z
+
+### Functions that return distance matrix corresponding to trajectories
 
 def trajectory_distance_weighted_velocities(positions, velocities, weight):
     assert(len(positions)>0)
@@ -81,11 +150,6 @@ def trajectory_distance_weighted_velocities_2Dtorus(positions, velocities, weigh
     distances_arr = np.array(distances_list)
     return np.min(distances_arr, axis=0)
 
-def compute_distance_matrices_timesteps_corridor(X, Y, vel_X, vel_Y, weight, length):
-    Dist_X = distances_corridor_weighted_velocities(X, vel_X, weight, length)
-    Dist_Y = distances_corridor_weighted_velocities(Y, vel_Y, weight, length)
-    Dist_Z = np.minimum(Dist_X, Dist_Y)
-    return Dist_X, Dist_Y, Dist_Z
 
 def compute_distance_matrices_trajectories_corridor(positions, velocities, start_step, shift_time, weight, length):
     half_shift=int(shift_time/2)
--- perdiver/perdiver.py
+++ perdiver/perdiver.py
@@ -2,22 +2,31 @@ from perdiver.distances import *
 
 import matplotlib as mpl
 
-import iblofunmatch.inter as ibfm
-output_dir="output"
+import tdqual.topological_data_quality_0 as tdqual
 
-def get_matching_diagram(Dist_X, Dist_Y, Dist_Z, output_dir):
-    # Compute X, Z barcodes and matching
-    idx_S = list(range(Dist_X.shape[0]))
-    # Compute matching from X to Z
-    ibfm_out = ibfm.get_IBloFunMatch_subset(Dist_X, Dist_Z, idx_S, output_dir, max_rad=-1, num_it=1, store_0_pm=True, points=False, max_dim=1)
+def get_matching_diagram(Dist_X, Dist_Y):
+    # Compute matching from X to Y
+    filt_X, filt_Y, matching = tdqual.compute_Mf_0(Dist_X, Dist_Y, is_dist=True)
     # Plot 0 persistence diagram of matching 
     match_diagram = []
-    for idx, bar_X in enumerate(ibfm_out["S_barcode_0"]):
-        idx_match = ibfm_out["induced_matching_0"][idx]
-        bar_Z = ibfm_out["X_barcode_0"][idx_match]
-        match_diagram.append([bar_X[1], bar_Z[1]])
+    for idx, idx_match in enumerate(matching):
+        match_diagram.append([filt_X[idx], filt_Y[idx_match]])
     # end for
     return np.array(match_diagram)
+
+# def get_matching_diagram(Dist_X, Dist_Y, Dist_Z, output_dir):
+#     # Compute X, Z barcodes and matching
+#     idx_S = list(range(Dist_X.shape[0]))
+#     # Compute matching from X to Z
+#     ibfm_out = ibfm.get_IBloFunMatch_subset(Dist_X, Dist_Z, idx_S, output_dir, max_rad=-1, num_it=1, store_0_pm=True, points=False, max_dim=1)
+#     # Plot 0 persistence diagram of matching 
+#     match_diagram = []
+#     for idx, bar_X in enumerate(ibfm_out["S_barcode_0"]):
+#         idx_match = ibfm_out["induced_matching_0"][idx]
+#         bar_Z = ibfm_out["X_barcode_0"][idx_match]
+#         match_diagram.append([bar_X[1], bar_Z[1]])
+#     # end for
+#     return np.array(match_diagram)
 
 def plot_matching_diagram(match_diagram, ax, color="blue", print_barcode_n_reps=False, max_val_diag=-1):
     # Plot matching diagram
@@ -84,23 +93,11 @@ def plot_sequence(X_list, ax, mark_points=[], color="red"):
         X_old = X
     #end for 
 
-def compute_divergence_vector(Dist_X, Dist_Y, Dist_Z):
+def compute_divergence_vector(Dist_X, Dist_Y):
     assert(Dist_X.shape[0]==Dist_Y.shape[0])
-    assert(Dist_X.shape[0]==Dist_Z.shape[0])
-    idx_S = list(range(int(Dist_X.shape[0])))
     # Compute matching
-    ibfm_out = [
-        ibfm.get_IBloFunMatch_subset(Dist_X, Dist_Z, idx_S, output_dir, max_rad=-1, num_it=1, store_0_pm=True, points=False, max_dim=1),
-        ibfm.get_IBloFunMatch_subset(Dist_Y, Dist_Z, idx_S, output_dir, max_rad=-1, num_it=1, store_0_pm=True, points=False, max_dim=1)
-    ]
-    # Compute induced matchings
-    matching_XZ = ibfm_out[0]["induced_matching_0"]
-    matching_YZ = ibfm_out[1]["induced_matching_0"]
-    composition_XY = [matching_YZ.index(i) for i in matching_XZ]
-    endpoints_0 = np.array(ibfm_out[0]["S_barcode_0"][:,1])
-    endpoints_1 = np.array(ibfm_out[1]["S_barcode_0"][:,1])
-    endpoints_1 = endpoints_1[composition_XY]
-    return  endpoints_1-endpoints_0, ibfm_out[0]["X_barcode_0"][:,1]
+    matching_diagram = get_matching_diagram(Dist_X, Dist_Y)
+    return  matching_diagram[:,1] - matching_diagram[:,0]
 
 def compute_divergence_Z_arrays(positions, velocities, steps_list, shift_step, weight, side):
     divergence_list = []
@@ -124,27 +121,27 @@ def compute_cumulative_array(div_arr):
         cumulative_list.append(cumulative)
     return np.array(cumulative_list).transpose()
 
-def plot_divergence_diagram(Dist_X, Dist_Y, Dist_Z, ax):
-    idx_S = list(range(int(Dist_X.shape[0])))
-    # Compute induced matchings
-    ibfm_out = [
-        ibfm.get_IBloFunMatch_subset(Dist_X, Dist_Z, idx_S, output_dir, max_rad=-1, num_it=1, store_0_pm=True, points=False, max_dim=1),
-        ibfm.get_IBloFunMatch_subset(Dist_Y, Dist_Z, idx_S, output_dir, max_rad=-1, num_it=1, store_0_pm=True, points=False, max_dim=1)
-    ]
-    # Divergence diagrams 
-    ibfm.plot_XYZ_matching_0(ibfm_out, ax)
-    ax.set_xlim([-4,4])
-    # print persistence divergence 
-    matching_XZ = ibfm_out[0]["induced_matching_0"]
-    matching_YZ = ibfm_out[1]["induced_matching_0"]
-    composition_XY = [matching_YZ.index(i) for i in matching_XZ]
-    endpoints_0 = np.array(ibfm_out[0]["S_barcode_0"][:,1])
-    endpoints_1 = np.array(ibfm_out[1]["S_barcode_0"][:,1])
-    endpoints_1 = endpoints_1[composition_XY]
-    persistence_divergence = np.sum(np.sqrt((endpoints_0-endpoints_1)**2))
-    # print(np.abs(endpoints_0-endpoints_1))
-    # print(persistence_divergence)
-    return ibfm_out
+# def plot_divergence_diagram(Dist_X, Dist_Y, Dist_Z, ax):
+#     idx_S = list(range(int(Dist_X.shape[0])))
+#     # Compute induced matchings
+#     ibfm_out = [
+#         ibfm.get_IBloFunMatch_subset(Dist_X, Dist_Z, idx_S, output_dir, max_rad=-1, num_it=1, store_0_pm=True, points=False, max_dim=1),
+#         ibfm.get_IBloFunMatch_subset(Dist_Y, Dist_Z, idx_S, output_dir, max_rad=-1, num_it=1, store_0_pm=True, points=False, max_dim=1)
+#     ]
+#     # Divergence diagrams 
+#     ibfm.plot_XYZ_matching_0(ibfm_out, ax)
+#     ax.set_xlim([-4,4])
+#     # print persistence divergence 
+#     matching_XZ = ibfm_out[0]["induced_matching_0"]
+#     matching_YZ = ibfm_out[1]["induced_matching_0"]
+#     composition_XY = [matching_YZ.index(i) for i in matching_XZ]
+#     endpoints_0 = np.array(ibfm_out[0]["S_barcode_0"][:,1])
+#     endpoints_1 = np.array(ibfm_out[1]["S_barcode_0"][:,1])
+#     endpoints_1 = endpoints_1[composition_XY]
+#     persistence_divergence = np.sum(np.sqrt((endpoints_0-endpoints_1)**2))
+#     # print(np.abs(endpoints_0-endpoints_1))
+#     # print(persistence_divergence)
+#     return ibfm_out
 
 def plot_matched_barcodes(Dist_X, Dist_Z, ax, fig):
     idx_S = list(range(int(Dist_X.shape[0])))
@@ -161,3 +158,13 @@ def plot_two_timesteps(X, Y, ax, X_col="blue", Y_col="red"):
     for edge in zip(X, Y):
         edge_pts = np.array(edge)
         ax.plot(edge_pts[:,0], edge_pts[:,1], c="gray", zorder=1)
+
+def plot_two_timesteps_with_velocities(X, Y, vel_X, vel_Y, ax, X_col="blue", Y_col="red"):
+    # Plot two timesteps and connecting segment
+    plot_two_timesteps(X, Y, ax, X_col=X_col, Y_col=Y_col)
+    # Draw velocities 
+    for pos, vel in zip(X, vel_X):
+        ax.arrow(pos[0], pos[1], vel[0], vel[1], color=X_col, zorder=2, width=0.05)
+    
+    for pos, vel in zip(Y, vel_Y):
+        ax.arrow(pos[0], pos[1], vel[0], vel[1], color=Y_col, zorder=2, width=0.05)
